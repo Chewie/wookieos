c067f1a0827eb4ed11da0ad9a9905061c8bc8e4b
Kévin Sztern <ksztern@octo.com>
2018-08-09 01:04:31 +0200

    Debug prints

--- kernel/main.c
+++ kernel/main.c
@@ -7,7 +7,7 @@
 #include "printf.h"
 
 
-void print_mmap(multiboot_info_t *mbi)
+static void print_mmap(multiboot_info_t *mbi)
 {
   multiboot_memory_map_t *mmap;
   printf("mmap_addr = 0x%x, mmap_length = 0x%x\n",
@@ -26,16 +26,44 @@ void print_mmap(multiboot_info_t *mbi)
            (unsigned) mmap->type);
 }
 
+static void print_modules(multiboot_info_t *mbi)
+{
+  printf("mods_count = %d, mods_addr = 0x%x\n", mbi->mods_count, mbi->mods_addr);
+  multiboot_module_t *curr_mod = (multiboot_module_t*)mbi->mods_addr;
+  for (size_t i = 0;
+       i < mbi->mods_count;
+       i++, curr_mod++)
+  {
+    printf(" start = 0x%x end = 0x%x cmdline = %s\n",
+           curr_mod->mod_start,
+           curr_mod->mod_end,
+           (char*)curr_mod->cmdline);
+  }
+}
+
+extern void *_end[];
 
 void kernel_main(uint32_t magic, multiboot_info_t *mbi)
 {
   (void) magic;
-  (void) mbi;
 
   serial_init();
   vga_init();
 
-  print_mmap(mbi);
+  if (!(mbi->flags & MULTIBOOT_INFO_MEM_MAP))
+  {
+    printf("[ERR] Multiboot memory map info unavailable!\n");
+    return;
+  }
+  else
+    print_mmap(mbi);
+
+  if (!(mbi->flags & MULTIBOOT_INFO_MODS))
+    printf("[INFO] No multiboot module info available\n");
+  else
+    print_modules(mbi);
+
+  printf("_end = 0x%x\n", _end);
 
 
   for (;;)
